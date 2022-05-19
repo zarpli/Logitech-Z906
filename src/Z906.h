3c6a693a21548b3f8b49b18e7459213006dc29fa
zarpli <43631223+zarpli@users.noreply.github.com>
2022-05-19 17:34:50 -0500

    Update Z906.h

--- src/Z906.h
+++ src/Z906.h
@@ -41,42 +41,41 @@
 #define	MUTE_ON           0x38
 #define	MUTE_OFF          0x39
 
-#define BLOCK_INPUTS		  0x22
-#define RESET_PWR_UP_TIME	0x30
-#define NO_BLOCK_INPUTS		0x33
+#define BLOCK_INPUTS      0x22
+#define RESET_PWR_UP_TIME 0x30
+#define NO_BLOCK_INPUTS   0x33
 
 // Double commmands
 
-#define MAIN_LEVEL			  0x03
-#define READ_LEVEL			  0x04
-#define CENTER_LEVEL		  0x05
-#define SUB_LEVEL			    0x06
+#define MAIN_LEVEL        0x03
+#define READ_LEVEL        0x04
+#define CENTER_LEVEL      0x05
+#define SUB_LEVEL         0x06
 
 // Requests
 
-#define VERSION				    0xF0
-#define CURRENT_INPUT		  0xF1
-#define GET_INPUT_GAIN		0x2F
-#define GET_TEMP			    0x25
-#define GET_PWR_UP_TIME		0x31
-#define GET_STATUS			  0x34
+#define VERSION           0xF0
+#define CURRENT_INPUT     0xF1
+#define GET_INPUT_GAIN    0x2F
+#define GET_TEMP          0x25
+#define GET_PWR_UP_TIME   0x31
+#define GET_STATUS        0x34
 
 // MASK
 
-#define EFFECT_3D			    0x00			
-#define EFFECT_21			    0x01			
-#define EFFECT_41			    0x02			
-#define EFFECT_NO			    0x03			
+#define EFFECT_3D         0x00			
+#define EFFECT_21         0x01			
+#define EFFECT_41         0x02			
+#define EFFECT_NO         0x03			
 
-#define SPK_NONE			    0x00			
-#define SPK_ALL				    0xFF
-#define SPK_FR				    0x01
-#define SPK_FL				    0x10
-#define SPK_RR				    0x02
-#define SPK_RL				    0x08
-#define SPK_CENTER			  0x04
-#define	SPK_SUB				    0x20
-
+#define SPK_NONE          0x00			
+#define SPK_ALL           0xFF
+#define SPK_FR            0x01
+#define SPK_FL            0x10
+#define SPK_RR            0x02
+#define SPK_RL            0x08
+#define SPK_CENTER        0x04
+#define	SPK_SUB           0x20
 
 class Z906
 {
@@ -93,33 +92,33 @@ uint8_t main_sensor();
 
 private:
 
-const uint8_t EXP_STX				= 	0xAA;
-const uint8_t EXP_MODEL_STATUS		= 	0x0A;
-const uint8_t EXP_MODEL_TEMP		= 	0x0C;
+const uint8_t EXP_STX               = 0xAA;
+const uint8_t EXP_MODEL_STATUS      = 0x0A;
+const uint8_t EXP_MODEL_TEMP        = 0x0C;
 
-const uint8_t STATUS_STX			=	0x00;
-const uint8_t STATUS_MODEL			=	0x01;
-const uint8_t STATUS_LENGTH			=	0x02;
-const uint8_t STATUS_MAIN_LEVEL		=	0x03;
-const uint8_t STATUS_READ_LEVEL		=	0x04;
-const uint8_t STATUS_CENTER_LEVEL	=	0x05;
-const uint8_t STATUS_SUB_LEVEL		=	0x06;
-const uint8_t STATUS_CURRENT_INPUT	=	0x07;
-const uint8_t STATUS_UNKNOWN		=	0x08;
-const uint8_t STATUS_FX_INPUT_4		=	0x09;
-const uint8_t STATUS_FX_INPUT_5		=	0x0A;
-const uint8_t STATUS_FX_INPUT_2		=	0x0B;
-const uint8_t STATUS_FX_INPUT_AUX	=	0x0C;
-const uint8_t STATUS_FX_INPUT_1		=	0x0D;
-const uint8_t STATUS_FX_INPUT_3		=	0x0E;
-const uint8_t STATUS_SPDIF_STATUS	=	0x0F;
-const uint8_t STATUS_SIGNAL_STATUS	=	0x10;
-const uint8_t STATUS_VER_A			=	0x11;
-const uint8_t STATUS_VER_B			=	0x12;
-const uint8_t STATUS_VER_C			=	0x13;
-const uint8_t STATUS_STBY			=	0x14;
-const uint8_t STATUS_AUTO_STBY		=	0x15;
-const uint8_t STATUS_CHECKSUM		=	0x16;
+const uint8_t STATUS_STX            = 0x00;
+const uint8_t STATUS_MODEL          =	0x01;
+const uint8_t STATUS_LENGTH         =	0x02;
+const uint8_t STATUS_MAIN_LEVEL     =	0x03;
+const uint8_t STATUS_READ_LEVEL     =	0x04;
+const uint8_t STATUS_CENTER_LEVEL   =	0x05;
+const uint8_t STATUS_SUB_LEVEL      =	0x06;
+const uint8_t STATUS_CURRENT_INPUT  =	0x07;
+const uint8_t STATUS_UNKNOWN        =	0x08;
+const uint8_t STATUS_FX_INPUT_4     =	0x09;
+const uint8_t STATUS_FX_INPUT_5     =	0x0A;
+const uint8_t STATUS_FX_INPUT_2     =	0x0B;
+const uint8_t STATUS_FX_INPUT_AUX   =	0x0C;
+const uint8_t STATUS_FX_INPUT_1     =	0x0D;
+const uint8_t STATUS_FX_INPUT_3     =	0x0E;
+const uint8_t STATUS_SPDIF_STATUS   =	0x0F;
+const uint8_t STATUS_SIGNAL_STATUS  =	0x10;
+const uint8_t STATUS_VER_A          =	0x11;
+const uint8_t STATUS_VER_B          =	0x12;
+const uint8_t STATUS_VER_C          =	0x13;
+const uint8_t STATUS_STBY           =	0x14;
+const uint8_t STATUS_AUTO_STBY      =	0x15;
+const uint8_t STATUS_CHECKSUM       =	0x16;
 
 HardwareSerial* dev_serial;
 
